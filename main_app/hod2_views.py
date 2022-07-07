1a2714e53cbc38d70127514b6e91a1b1afb8b997
Mubin-Hardy <mubinmattel@gmail.com>
2022-07-07 16:49:04 +0530

    cn

--- main_app/hod2_views.py
+++ main_app/hod2_views.py
@@ -28,26 +28,30 @@ def manage_staff_hod2(request):
     return render(request, "hod2_template/manage_staff_hod2.html", context)
 
 def hod2_take_attendance(request):
-    allStaff = CustomUser.objects.filter(user_type=2)
-    #.filter(cl=cl)
-    aform=AttendanceStaffForm()
     if request.method=='POST':
-        form=AttendanceStaffForm(request.POST)
-        if form.is_valid():
-            Attendances=request.POST.getlist('present_status')
-            date=form.cleaned_data['date']
-            for i in range(len(Attendances)):
-                AttendanceModel=AttendanceStaff()
+        dept = request.POST.get('departement')
+        allStaff = Staff.objects.filter(course__name__startswith=dept)
+        aform=AttendanceStaffForm()
+        if request.method=='POST':
+            form=AttendanceStaffForm(request.POST)
+            if form.is_valid():
+                Attendances=request.POST.getlist('present_status')
+                date=form.cleaned_data['date']
+                for i in range(len(Attendances)):
+                    AttendanceModel=AttendanceStaff()
                 #AttendanceModel.cl=cl
-                AttendanceModel.date=date
-                AttendanceModel.present_status=Attendances[i]
+                    AttendanceModel.date=date
+                    AttendanceModel.present_status=Attendances[i]
                 #AttendanceModel.course=allStaff[i].course
-                AttendanceModel.save()
-            return redirect('hod2_take_attendance')
-        else:
-            print('form invalid')
-    return render(request,'hod2_template/hod2_take_attendance.html',{'staffs':allStaff,'aform':aform})
-
+                    AttendanceModel.save()
+                return redirect('hod2_take_attendance')
+            else:
+                print('form invalid')
+        print(aform)
+        mylist = zip(allStaff, aform)
+        return render(request,'hod2_template/hod2_take_attendance.html',{'mylist':mylist,'staffs':allStaff,'aform':aform})
+    return render(request,'hod2_template/hod2_take_attendance.html')
+#return render(request,'hod2_template/hod2_take_attendance.html',{'staffs':allStaff,'aform':aform})
 
 # def hod2_view_attendance(request):
 #     form=AskDateForm()
@@ -89,27 +93,10 @@ def hod2_searchdept(request):
         dept = request.POST.get('departement')
         j = str(dept)
         print(j)
-        #course = Staff.objects.filter(course__name__startswith=j)
-        print("ok")
-    allStaff = CustomUser.objects.filter(user_type=2)
-    staff = Staff.objects.filter(course_id=1)
-    print(staff)
-    #.filter(cl=cl)
-    aform=AttendanceStaffForm()
-    if request.method=='POST':
-        form=AttendanceStaffForm(request.POST)
-        if form.is_valid():
-            Attendances=request.POST.getlist('present_status')
-            date=form.cleaned_data['date']
-            for i in range(len(Attendances)):
-                AttendanceModel=AttendanceStaff()
-                #AttendanceModel.cl=cl
-                AttendanceModel.date=date
-                AttendanceModel.present_status=Attendances[i]
-                #AttendanceModel.course=allStaff[i].course
-                AttendanceModel.save()
-            return redirect('hod2_take_attendance')
-        else:
-            print('form invalid')
-    return render(request,'hod2_template/hod2_take_attendance.html',{'staffs':allStaff,'aform':aform})
+        course = Staff.objects.filter(course__name__startswith=j)
+        aform=AttendanceStaffForm()
+        return render(request,'hod2_template/hod2_take_search.html',{'course':course})
         
+    else:
+        print("not ok")
+        return render(request,'hod2_template/home_content.html')
