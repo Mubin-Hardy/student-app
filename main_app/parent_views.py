676d8e0247241844e2dde72f78258b380a3864c6
Mubin-Hardy <mubinmattel@gmail.com>
2022-07-09 11:09:47 +0530

    dghft

--- main_app/parent_views.py
+++ main_app/parent_views.py
@@ -21,25 +21,29 @@ def parent_home(request):
 
 def parent_due(request):
     if request.method=='POST':
-        stud = request.POST.get('student')
-        parent = get_object_or_404(Parent, admin=request.user)
-        student = Student.objects.all().filter(username=stud)
-        print(student)
-        field_name1 = 'student_fee'
-        field_name2 = 'student_fee_paid'
-        obj = Student.objects.first()
-        field_value1 = getattr(obj, field_name1)
-        field_value2 = getattr(obj, field_name2)
-        tot = field_value1 - field_value2
+        try:
+            stud = request.POST.get('student')
+            parent = get_object_or_404(Parent, admin=request.user)
+            student = Student.objects.all().filter(username=stud)
+            print(student)
+            field_name1 = 'student_fee'
+            field_name2 = 'student_fee_paid'
+            obj = Student.objects.filter(username=stud).first()
+            field_value1 = getattr(obj, field_name1)
+            field_value2 = getattr(obj, field_name2)
+            tot = field_value1 - field_value2
         
         
         
         
         
-        context = {
-            'page_title': 'Parent Portal',
-            'student' : student,
-            'tot':tot
-        }
-        return render(request, 'parent_template/parent_due.html', context)
+            context = {
+                'page_title': 'Parent Portal',
+                'student' : student,
+                'tot':tot
+            }
+            return render(request, 'parent_template/parent_due.html', context)
+        except Exception as e:
+            messages.error(request, "Admin not Updated Student Fee details " + str(e))
+            return render(request, 'parent_template/parent_due.html')
     return render(request, 'parent_template/parent_due.html')
